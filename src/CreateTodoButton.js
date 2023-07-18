f27be770cee7b59f6c4fb08f445a4072b98f9697
Yordi <superyo234@gmail.com>
2023-07-18 11:57:30 -0600

    Cambios visuales

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -4,7 +4,10 @@ import { AiFillPlusCircle } from "react-icons/ai"
 function CreateTodoButton(){
     return(
         <button>
-            < AiFillPlusCircle id='plusButton' style={{marginTop: '5px'}} /> 
+            <AiFillPlusCircle id='plusButton' style={{marginTop: '5px'}} 
+            onClick={(event)=>{console.log("Click en el boton")
+            console.log(event)
+            console.log(event.target)}}/> 
         </button>
     );
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,7 +3,10 @@ import './TodoSearch.css'
 function TodoSearch () {
     return (
       <>
-      <input placeholder="Cortar Cebolla" />
+      <input placeholder="Cortar Cebolla"  onChange={(event)=>{console.log("Cambiaste el texto")
+            console.log(event)
+            console.log(event.target)
+            console.log(event.target.value)}} />
       <br />
       <hr />
       </>
